ff81cd76f54d5bfc06a6873a9a75f7d7da7e642a
Olivier Lombart <kubicle@yahoo.com>
2013-08-15 17:57:22 +0900

    Just adapted to change in set_player parameters

--- main_console.rb
+++ main_console.rb
@@ -1,10 +1,11 @@
+require_relative "logging"
 require_relative "controller"
 require_relative "ai1_player"
 require_relative "human_player"
 
 c = Controller.new(9,2,0)
-# c.set_player(0, Ai1Player.new)
-# c.set_player(1, Ai1Player.new)
-c.set_player(0, HumanPlayer.new)
-c.set_player(1, HumanPlayer.new)
+# c.set_player(0, Ai1Player)
+# c.set_player(1, Ai1Player)
+c.set_player(0, HumanPlayer)
+c.set_player(1, HumanPlayer)
 c.play_console_game
