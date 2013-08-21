39c4f9cb47b654f972c7c8d3611825569bf83d2c
kubicle <kubicle@yahoo.com>
2013-08-21 12:40:02 +0900

    Debug level logging as default makes sense

--- logging.rb
+++ logging.rb
@@ -3,8 +3,8 @@ require "logger"
 $log = Logger.new(STDOUT)
 
 # change $log.level to Logger::DEBUG, etc. as you need
-$log.level=Logger::INFO
+$log.level=Logger::DEBUG
 
 # change $debug to true to see all the debug logs
 # NB: note this slows down everything if $debug is true even if the log level is not DEBUG
-$debug = false 
+$debug = true
