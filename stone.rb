896ce5b950252d4b7e2996f5905333040638ebe6
kubicle <kubicle@yahoo.com>
2013-08-21 18:37:19 +0900

    Removed Stone.around (simpler & more efficient)

--- stone.rb
+++ stone.rb
@@ -14,7 +14,7 @@ class Stone
   @@color_names = ["black","white","red","blue"] # not constant as this could be user choice
   @@num_colors = 2 # default in real world; I would like to see a game with 3 or 4 one day though :)
   
-  attr_reader :goban, :group, :color, :i, :j, :neighbors, :around
+  attr_reader :goban, :group, :color, :i, :j, :neighbors
   
   def Stone.init(num_colors)
     raise "Max player number is #{@@color_names.size}" if num_colors>@@color_names.size
@@ -29,20 +29,18 @@ class Stone
     @group = nil
     # @neighbors contains the neighboring stones (empty or not); no need to compute coordinates anymore
     @neighbors = Array.new(4)
-    @neighbors.clear
-    # @around contains everything around a stone: unique groups (per color) and empty stones
-    @around = Array.new(@@num_colors+1) {[]} # +1 for since EMPTY is the last "color"
+    # @allies and @enemies are used as buffers for corresonding methods (unique_allies, unique_enemies etc.)
+    @allies = Array.new(4)
+    @enemies = Array.new(4)
   end
 
   # Computes each stone's neighbors (called for each stone after init)
   # NB: Stones next to side have only 3 neighbors, and the corner stones have 2
   def find_neighbors
+    @neighbors.clear
     XY_AROUND.each do |coord_change|
       stone = goban.stone_at?(@i+coord_change[0], @j+coord_change[1])
-      if stone != BORDER then
-        @neighbors.push(stone)
-        @around[stone.color].push(stone)
-      end
+      @neighbors.push(stone) if stone != BORDER
     end
   end
   
@@ -50,29 +48,31 @@ class Stone
     "stone#{to_text}:#{as_move}"
   end
   
+  # Returns "c3" for a stone in 3,3
   def as_move
     "#{Goban.move_as_string(@i,@j)}"
   end
   
   def debug_dump
-    s = to_s
-    s << " around: "
-    EMPTY.upto(@@num_colors-1) do |color|
-      s << " #{COLOR_CHARS[color]}["
-      @around[color].each do |item|
-        s << "#{item.as_move} " if item.is_a?(Stone)
-        s << "##{item.ndx} "if item.is_a?(Group)
-      end
-      s.chop! if @around[color].size>0
-      s << "]"
-    end
-    return s
+    to_s # we could add more info
+  end
+
+  # As of now this is used for debug only  
+  def empties
+    @neighbors.select { |s| s.color == EMPTY }
+  end
+
+  # Returns a string with the list of lives, sorted (debug only)
+  def lives_dump
+    return empties.map{|s| s.as_move}.sort.join(",")
   end
   
+  # Returns the "character" used to represent a stone in text style
   def to_text
     return COLOR_CHARS[@color]
   end
 
+  # Returns the name of the color/player for this stone (e.g. "black")
   def Stone.color_name(color)
     return @@color_names[color]
   end
@@ -82,7 +82,7 @@ class Stone
   end
 
   def Stone.valid_move?(goban, i, j, color)
-    # # $log.debug("Stone.valid_move? #{i}, #{j}, color:#{color}") if $debug
+    # Remark: no log here because of the noise created with web server mode
     return false if !goban.valid_move?(i,j) # also checks if empty
 
     stone = goban.stone_at?(i,j)
@@ -96,12 +96,14 @@ class Stone
   # or if one enemy group will be killed
   # or if the result of the merge of ally groups will have more than 0 life
   def move_is_suicide?(color)
-    return false if @around[EMPTY].size != 0
-    
-    each_enemy(color) { |enemy| return false if enemy.lives == 1 }
-    
-    @around[color].each { |ally| return false if ally.lives > 1 }
-    
+    @neighbors.each do |s|
+      return false if s.color == EMPTY
+      if s.color != color
+        return false if s.group.lives == 1
+      else
+        return false if s.group.lives > 1
+      end
+    end
     $log.debug("move #{@i}, #{@j}, color:#{color} would be a suicide") if $debug
     return true
   end
@@ -111,21 +113,25 @@ class Stone
   # and the previous move killed with stone A a single stone B in same position i,j
   # then it is a ko
   def move_is_ko?(color)
-    group_a,kill_count = nil,0
-    each_enemy(color) { |enemy| group_a,kill_count = enemy,kill_count+1 if enemy.lives == 1 }
-    return false if kill_count != 1
+    # Must kill a single group
+    group_a = nil
+    each_enemy(color) { |enemy| next if enemy.lives != 1; return false if group_a; group_a = enemy }
+    return false if ! group_a
+    # This killed group must be a single stone A
     return false if group_a.stones.size != 1
     stone_a = group_a.stones.first
+    # Stone A was played just now
     return false if @goban.previous_stone != stone_a
     
+    # Stone B was killed by A in same position we are looking at
     group_b = @goban.killed_groups.last
     return false if group_b.killed_by != stone_a
     return false if group_b.stones.size != 1
     stone_b = group_b.stones.first
-    return false if stone_b.i != i or stone_b.j != j
+    return false if stone_b.i != @i or stone_b.j != @j
 
     $log.debug("ko in #{@i}, #{@j}, color:#{color} cannot be played now") if $debug
-    return true # a ko!
+    return true
   end
 
   def Stone.play_at(goban,i,j,color)
@@ -133,16 +139,23 @@ class Stone
     stone.put_down(color)
     return stone
   end
+  
+  # Wil be used for various evaluations (currently for filling a zone)
+  # color should not be a player's color nor EMPTY unless we do not plan to 
+  # continue the game on this goban (or we plan to restore everything we marked)
+  def mark_a_spot!(color) # TODO refactor me
+    @color = color
+  end
 
   def die
-    update_around_before_die
+    # update_around_before_die
     @color = EMPTY
   end
   
   def resuscitate(group)
     @group = group
     @color = group.color
-    update_around_on_new
+    # update_around_on_new
   end
 
   # Called to undo a single stone (the main undo feature relies on this)  
@@ -153,91 +166,76 @@ class Stone
     stone.take_back 
   end
   
-  # Iterate through enemy groups
+  # Iterate through enemy groups (same group can appear more than once)
   # This is simply done by going through all colors but EMPTY and the ally (our own color)
   def each_enemy(ally_color)
-    0.upto(@@num_colors - 1) do |color|
-      next if color == ally_color
-      @around[color].each do |enemy|
-        raise "Unexpected error (dead or merged enemy)" if enemy.merged_by or enemy.killed_by
-        yield enemy
-      end
+    @neighbors.each { |s| yield s.group if s.color != EMPTY and s.color != ally_color }
+  end
+
+  def unique_enemies(ally_color)
+    @enemies.clear
+    @neighbors.each do |s|
+      @enemies.push(s.group) if s.color != EMPTY and s.color != ally_color and ! @enemies.find_index(s.group)
     end
+    return @enemies
+  end
+
+  def each_ally(ally_color)
+    @neighbors.each { |s| yield s.group if s.color == ally_color }
+  end
+
+  def unique_allies(color)
+    @allies.clear
+    @neighbors.each do |s|
+      @allies.push(s.group) if s.color == color and ! @allies.find_index(s.group)
+    end
+    return @allies
   end
 
   # Called for each new stone played
   def put_down(color)
     @color = color
-    allies = @around[color]
+    allies = unique_allies(color) # note we would not need unique if group#merge ignores dupes
     if allies.size == 0
-      @group = Group.new(@goban,self,@around[EMPTY].size)
+      lives = 0
+      @neighbors.each { |s| lives += 1 if s.color == EMPTY }
+      @group = Group.new(@goban,self,lives)
     else
       @group = allies.first
       @group.connect_stone(self)
       1.upto(allies.size-1) { |a| @group.merge(allies[a],self) }
     end
-    update_around_on_new
-    each_enemy(color) { |g| g.attacked_by(self) }
+    # update_around_on_new
+    unique_enemies(color).each { |g| g.attacked_by(self) }
   end
 
   def take_back()
-    while @goban.merged_groups.last().merged_by == self do
-      ally = @goban.merged_groups.last()
-      $log.debug("take_back: about to unmerge "+ally.to_s) if $debug
-      @group.unmerge(ally)
-    end
-    $log.debug("take_back: about to disconnect "+self.to_s) if $debug
+    $log.debug("take_back: #{to_s} from group #{@group}") if $debug
+    @group.unmerge_from(self)
     @group.disconnect_stone(self)
-    each_enemy(@color) { |g| g.not_attacked_anymore(self) }
-    update_around_before_die
-    $log.debug("take_back: end; main group: #{@group.debug_dump}") if $debug
+    unique_enemies(@color).each { |g| g.not_attacked_anymore(self) }
 
+    # update_around_before_die
+    log_group = @group if $debug
     @group = nil
     @color = EMPTY
 
-    while @goban.killed_groups.last().killed_by == self do
-      enemy = @goban.killed_groups.last()
-      $log.debug("take_back: about to resuscitate "+enemy.to_s) if $debug
-      enemy.resuscitate()
-    end
+    Group.resuscitate_from(self,@goban)
+    $log.debug("take_back: end; main group: #{log_group.debug_dump}") if $debug
   end
   
-  def update_around_on_new
-    @neighbors.each do |s|
-      s.around[EMPTY].delete(self)
-      a = s.around[@color]
-      a.push(@group) if ! a.find_index(@group)
-    end
-  end
-
-  def update_around_before_die
-    $log.debug("update_around_before_die #{self.debug_dump} removing ##{@group.ndx} from around refs") if $debug
-    @neighbors.each do |s|
-      s.around[EMPTY].push(self)
-      a = s.around[@color]
-      if (ndx = a.find_index(@group))
-        $log.debug("update_around_before_die: removing ##{@group.ndx} from #{s}") if $debug
-        other_neighbor = false
-        s.neighbors.each do |stone|
-          if stone != self and stone.group == @group
-            other_neighbor = true
-            break
-          end
-        end
-        a.delete(@group) if ! other_neighbor
-        $log.debug("update_around_before_die: not removed since other neighbor exists") if $debug and other_neighbor
-      end # if ndx
-    end
-  end
-
   def set_group_on_merge(new_group)
-    # replace any reference to the old group by the new one
-    @neighbors.each do |s|
-      a = s.around[@color]
-      ndx = a.find_index(@group)
-      a[ndx] = new_group if ndx
-    end
     @group = new_group
   end
+
+  # Not used anymore but could become handy again later
+  # def update_around_on_new
+  #   $log.debug("update_around_on_new #{self.debug_dump}") if $debug
+  # end
+
+  # Not used anymore but could become handy again later
+  # def update_around_before_die
+  #   $log.debug("update_around_before_die #{self.debug_dump}") if $debug
+  # end
 
 end
