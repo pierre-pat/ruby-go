f66a9a0648277a46e9f816d932914958a29b5858
kubicle <kubicle@yahoo.com>
2013-08-23 03:17:34 +0900

    Made 2 classes for empty zones analysis
    Currently in util.rb; will move on with more changes

--- test/test_fill.rb
+++ test/test_fill.rb
@@ -1,8 +1,8 @@
 require 'test/unit'
 
-require_relative "../logging"
 require_relative "../controller"
 require_relative "../human_player"
+require_relative "../logging"
 require_relative "../util"
 
 # NB: for debugging think of using @goban.debug_display
@@ -18,6 +18,8 @@ class TestFill < Test::Unit::TestCase
     @controller.set_player(1, HumanPlayer)
     @goban = @controller.goban
     @@x = @goban.char_to_color("X") # we use this color for replacements
+    
+    @boan = BoardAnalyser.new(@goban,EMPTY)
   end
 
   def initialize(x)
@@ -25,7 +27,7 @@ class TestFill < Test::Unit::TestCase
     init_board()
   end
 
-  def test_fill1
+  def xtest_fill1
     # 5 +O+++
     # 4 +@+O+
     # 3 +O+@+
@@ -34,16 +36,16 @@ class TestFill < Test::Unit::TestCase
     #   abcde
     @goban.load_image("+O+++,+@+O+,+O+@+,+@+O+,+++@+")
     @goban.console_display
-    fill_with_color(@goban,3,1,@@x)
+    @boan.fill_with_color(3,1,@@x)
     @goban.console_display
     assert_equal("XOXXX,X@XOX,XOX@X,X@XOX,XXX@X", @goban.image?);
 
     @goban.load_image("+O+++,+@+O+,+O+@+,+@+O+,+++@+")
-    fill_with_color(@goban,1,3,@@x)
+    @boan.fill_with_color(1,3,@@x)
     assert_equal("XOXXX,X@XOX,XOX@X,X@XOX,XXX@X", @goban.image?);
   end
 
-  def test_fill2
+  def xtest_fill2
     # 5 +++++
     # 4 +OOO+
     # 3 +O+O+
@@ -52,19 +54,19 @@ class TestFill < Test::Unit::TestCase
     #   abcde
     @goban.load_image("+++++,+OOO+,+O+O+,+++O+,+OOO+")
     # @goban.console_display
-    fill_with_color(@goban,3,3,@@x)
+    @boan.fill_with_color(3,3,@@x)
     assert_equal("XXXXX,XOOOX,XOXOX,XXXOX,XOOOX", @goban.image?);
 
     @goban.load_image("+++++,+OOO+,+O+O+,+++O+,+OOO+")
-    fill_with_color(@goban,1,1,@@x)
+    @boan.fill_with_color(1,1,@@x)
     assert_equal("XXXXX,XOOOX,XOXOX,XXXOX,XOOOX", @goban.image?);
 
     @goban.load_image("+++++,+OOO+,+O+O+,+++O+,+OOO+")
-    fill_with_color(@goban,5,3,@@x)
+    @boan.fill_with_color(5,3,@@x)
     assert_equal("XXXXX,XOOOX,XOXOX,XXXOX,XOOOX", @goban.image?);
   end
 
-  def test_fill2
+  def xtest_fill3
     # 5 +++O+
     # 4 +++OO
     # 3 +O+++
@@ -73,20 +75,39 @@ class TestFill < Test::Unit::TestCase
     #   abcde
     @goban.load_image("+++O+,+++OO,+O+++,++OO+,+O+O+")
     @goban.console_display
-    fill_with_color(@goban,2,4,@@x)
+    @boan.fill_with_color(2,4,@@x)
     assert_equal("XXXO+,XXXOO,XOXXX,XXOOX,XO+OX", @goban.image?);
 
     @goban.load_image("+++O+,+++OO,+O+++,++OO+,+O+O+")
-    fill_with_color(@goban,2,2,@@x)
+    @boan.fill_with_color(2,2,@@x)
     assert_equal("XXXO+,XXXOO,XOXXX,XXOOX,XO+OX", @goban.image?);
 
     @goban.load_image("+++O+,+++OO,+O+++,++OO+,+O+O+")
-    fill_with_color(@goban,3,1,@@x)
+    @boan.fill_with_color(3,1,@@x)
     assert_equal("+++O+,+++OO,+O+++,++OO+,+OXO+", @goban.image?);
 
     @goban.load_image("+++O+,+++OO,+O+++,++OO+,+O+O+")
-    fill_with_color(@goban,5,5,@@x)
+    @boan.fill_with_color(5,5,@@x)
     assert_equal("+++OX,+++OO,+O+++,++OO+,+O+O+", @goban.image?);
   end
 
+  def test_empty_zones
+    init_board(9)
+    # 9 ++O@@++++
+    # 8 +@OO@++@+
+    # 7 OOOO@@@++
+    # 6 ++OOOOO@@
+    # 5 OO@@O@@@@
+    # 4 @@@+OOOO@
+    # 3 O@@@@@O+O
+    # 2 +++@OOO++
+    # 1 +++@@O+++
+    #   abcdefghi
+    game2 = "c3,c6,e7,g3,g7,e2,d2,b4,b3,c7,g5,h4,h5,d8,e8,e5,c4,b5,e3,f2,c5,f6,f7,g6,h6,d7,a4,a5,b6,a3,a6,b7,a4,a7,d9,c9,b8,e6,d5,d6,e9,g4,f5,f4,e1,f1,d1,i5,i6,e4,i4,i3,h8,c8,d3,i5,f3,g2,i4,b5,b4,a5,i5"
+    @controller.play_moves(game2)
+    final_pos = "++O@@++++,+@OO@++@+,OOOO@@@++,++OOOOO@@,OO@@O@@@@,@@@+OOOO@,O@@@@@O+O,+++@OOO++,+++@@O+++"
+    assert_equal(final_pos, @goban.image?);
+
+    @boan.analyse_empty_zones
+  end
 end
--- util.rb
+++ util.rb
@@ -1,41 +1,117 @@
-require_relative "stone_constants"
+require_relative "goban"
 
-def fill(goban, i, j, color)
-  $log.debug("fill #{i} #{j}") if $debug
-  return false if goban.color?(i,j) != EMPTY
-  gaps = [[i,j,j]]
-  while (gap = gaps.pop)
-    $log.debug("About to do gap: #{gap} (left #{gaps.size})") if $debug
-    i,j0,j1 = gap
-    while goban.color?(i,j0-1) == EMPTY do j0 -= 1 end
-    while goban.color?(i,j1+1) == EMPTY do j1 += 1 end
-    $log.debug("Doing column #{i} from #{j0}-#{j1}") if $debug
-    (i-1).step(i+1,2).each do |ix|
-      curgap = nil
-      j0.upto(j1) do |j|
-        $log.debug("=>coloring #{i},#{j}") if $debug and ix<i
-        goban.mark_a_spot!(i,j,color) if ix<i
-        $log.debug("checking neighbor #{ix},#{j}") if $debug
-        if goban.color?(ix,j) == EMPTY
-          if ! curgap
-            $log.debug("New gap in #{ix} starts at #{j}") if $debug
-            curgap = j # gap start
-          end
-        else
-          if curgap
-            $log.debug("--- pushing gap [#{ix},#{curgap},#{j-1}]") if $debug
-            gaps.push([ix,curgap,j-1])
-            curgap = nil
-          end
-        end
-      end # upto j
-      $log.debug("--- pushing gap [#{ix},#{curgap},#{j1}]") if $debug and curgap
-      gaps.push([ix,curgap,j1]) if curgap # last gap
-    end # each ix
-  end # while gap
-  return true
+class EmptyZone
+  attr_reader :neighbors
+  
+  def initialize(i,j,size,neighbors)
+    @i = i
+    @j = j
+    @size = size
+    @neighbors = neighbors
+  end
+  
+  def EmptyZone.new_neighbors
+    return [[],[]]
+  end
+  
+  def to_s # TODO with a loop
+    return "zone #{@i}#{@j}, size #{@size}, " +
+      "#{@neighbors[0].size} black neighbors, " + 
+      "#{@neighbors[1].size} white neighbors"
+  end
+
 end
 
-def fill_with_color(goban,i,j,color)
- fill(goban,i,j,color)
+class BoardAnalyser
+
+  # neighbors if given should be an array of n arrays, with n == number of colors
+  def initialize(goban, to_replace = EMPTY)
+    @goban = goban
+    @to_replace = to_replace
+    @first_empty_zone_code = 10 # anything above 5 is OK
+    @zones = []
+  end
+
+  # Returns true if the replacement is needed (=> i,j has a color equal to the replaced one)
+  def _check(i,j)
+    stone = @goban.stone_at?(i,j)
+    return false if ! stone
+    return true if stone.color == @to_replace
+    if @neighbors and stone.color < @first_empty_zone_code
+      @neighbors[stone.color].push(stone.group) if ! @neighbors[stone.color].find_index(stone.group)
+    end
+    return false
+  end
+
+  def fill_with_color(i, j, color)
+    $log.debug("fill #{i} #{j} with #{color}") if $debug
+    return 0 if @goban.color?(i,j) != @to_replace
+    size = 0
+    @neighbors = EmptyZone.new_neighbors
+    gaps = [[i,j,j]]
+    while (gap = gaps.pop)
+      $log.debug("About to do gap: #{gap} (left #{gaps.size})") if $debug
+      i,j0,j1 = gap
+      while _check(i,j0-1) do j0 -= 1 end
+      while _check(i,j1+1) do j1 += 1 end
+      size += j1-j0+1
+      $log.debug("Doing column #{i} from #{j0}-#{j1}") if $debug
+      (i-1).step(i+1,2).each do |ix|
+        curgap = nil
+        j0.upto(j1) do |j|
+          # $log.debug("=>coloring #{i},#{j}") if $debug and ix<i
+          @goban.mark_a_spot!(i,j,color) if ix<i
+          # $log.debug("checking neighbor #{ix},#{j}") if $debug
+          if _check(ix,j)
+            if ! curgap
+              # $log.debug("New gap in #{ix} starts at #{j}") if $debug
+              curgap = j # gap start
+            end
+          else
+            if curgap
+              # $log.debug("--- pushing gap [#{ix},#{curgap},#{j-1}]") if $debug
+              gaps.push([ix,curgap,j-1])
+              curgap = nil
+            end
+          end
+        end # upto j
+        # $log.debug("--- pushing gap [#{ix},#{curgap},#{j1}]") if $debug and curgap
+        gaps.push([ix,curgap,j1]) if curgap # last gap
+      end # each ix
+    end # while gap
+    return size
+  end
+
+  def analyse_empty_zones
+    zone_code = @first_empty_zone_code
+    1.upto(@goban.size) do |j|
+      1.upto(@goban.size) do |i|
+        if (size = fill_with_color(i,j,zone_code)) > 0
+          @zones.push(EmptyZone.new(i,j,size,@neighbors))
+          zone_code += 1
+        end
+      end
+    end
+    debug_dump if $debug
+  end
+    
+  def debug_dump  
+    print @goban._to_console { |s|
+      if s.color >= @first_empty_zone_code
+        ("A".ord + s.color - @first_empty_zone_code).chr
+      else
+        @goban.stone_to_text(s.color)
+      end
+    }
+    @zones.each do |zone|
+      puts "\nZone: #{zone}"
+      0.upto(1) do |color|
+        print "    Color #{color}:"
+        zone.neighbors[color].each do |neighbor|
+          print " ##{neighbor.ndx}"
+        end
+      end
+    end
+  end
+  
 end
