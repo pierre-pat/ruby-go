363e5ab48625242b6fa7fba0fd17372bcf792682
kubicle <kubicle@yahoo.com>
2013-08-22 14:51:38 +0900

    More clever speed test (not using game controller)

--- test/test_speed.rb
+++ test/test_speed.rb
@@ -35,10 +35,27 @@ class TestSpeed < Test::Unit::TestCase
 
   def test_speed
     tolerance = 1.20
+
+    # prepare games so we isolate the GC caused by that 
+    # (in real AI thinking there will be many other things but...)
+    # 35 moves, final position:
+    # 9 +++OO@+++
+    # 8 +@OO@@+++
+    # 7 +O+OO@+++
+    # 6 ++O@@++++
+    # 5 ++OO@++++
+    # 4 ++O+O@+++
+    # 3 ++O+O@+++
+    # 2 ++@OO@+@+
+    # 1 ++++OO@++
+    #   abcdefghi
+    game1 = "c3,f3,d7,e5,c5,f7,e2,e8,d8,f2,f1,g1,e1,h2,e3,d4,e4,f4,d5,d3,d2,c2,c4,d6,e7,e6,c6,f8,e9,f9,d9,c7,c8,b8,b7"
+    game1_moves_ij = moves_ij(game1)
+
     t = TimeKeeper.new(tolerance)
     t.calibrate
 
-    t.start("100,000 stones and undo", 3.84, 11)
+    t.start("100,000 stones (no validation) and undo", 3.84, 11)
     1.upto(10000) do
       play_10_stones
     end
@@ -47,19 +64,53 @@ class TestSpeed < Test::Unit::TestCase
 
     t.start("35 move game, 2000 times and undo", 4.60, 39)
     1.upto(2000) do
-      play_game_1
+      play_game_and_clean(game1_moves_ij)
     end
     t.stop
     show_count
 
+    # The idea here is to verify that undoing things is cheaper than throwing it all to GC
+    # In a tree exploration strategy the undo should be the only way (otherwise we quickly hog all memory)
     t.start("35 move game, 2000 times new board each time", 4.87, 26)
     1.upto(2000) do
-      play_game_1(false)
+      play_game_and_clean(game1_moves_ij,false)
     end
     t.stop
     show_count
   end
+  
+  # Converts "a1,b2" in [1,1,2,2]
+  def moves_ij(game)
+    return game.split(",").collect_concat { |m| Goban.parse_move(m) }
+  end
 
+  def play_moves(moves_ij)
+    move_count = 0
+    cur_color = BLACK
+    0.step(moves_ij.size - 2, 2) do |n|
+      i = moves_ij[n]
+      j = moves_ij[n+1]
+      raise "Invalid move generated: #{move}" if !Stone.valid_move?(@goban, i, j, cur_color)
+      Stone.play_at(@goban, i, j, cur_color)
+      move_count += 1
+      cur_color = (cur_color+1) % 2
+    end
+    return move_count
+  end
+
+  def play_game_and_clean(moves_ij, with_undo=true)
+    num_moves = moves_ij.size/2
+    assert_equal(num_moves, play_moves(moves_ij))
+
+    if with_undo then
+      1.upto(num_moves) { Stone.undo(@goban) }
+    else
+      init_board()
+    end
+    assert_equal(nil, @goban.previous_stone)
+  end
+  
+  # Our first, basic test
   def play_10_stones
     Stone.play_at(@goban, 2, 2, WHITE)
     Stone.play_at(@goban, 1, 2, BLACK)
@@ -72,26 +123,6 @@ class TestSpeed < Test::Unit::TestCase
     Stone.play_at(@goban, 5, 5, WHITE)
     Stone.play_at(@goban, 5, 4, BLACK)
     1.upto(10) { Stone.undo(@goban) }
-  end
-  
-  def play_game_1(with_undo=true)
-    # 35 moves, final position:
-    # 9 +++OO@+++
-    # 8 +@OO@@+++
-    # 7 +O+OO@+++
-    # 6 ++O@@++++
-    # 5 ++OO@++++
-    # 4 ++O+O@+++
-    # 3 ++O+O@+++
-    # 2 ++@OO@+@+
-    # 1 ++++OO@++
-    #   abcdefghi
-    @controller.play_moves("c3,f3,d7,e5,c5,f7,e2,e8,d8,f2,f1,g1,e1,h2,e3,d4,e4,f4,d5,d3,d2,c2,c4,d6,e7,e6,c6,f8,e9,f9,d9,c7,c8,b8,b7")
-    if with_undo then
-      1.upto(35) { Stone.undo(@goban) }
-    else
-      init_board()
-    end
   end
   
 end
