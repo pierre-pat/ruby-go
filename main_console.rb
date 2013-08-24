4ed93af3187783406e0a2da055275edc4a06fd57
kubicle <kubicle@yahoo.com>
2013-08-24 17:38:15 +0900

    Command line options for console game.
    A series of moves can be loaded from command line.

--- main_console.rb
+++ main_console.rb
@@ -1,11 +1,30 @@
+require 'trollop'
+
 require_relative "logging"
 require_relative "controller"
 require_relative "ai1_player"
 require_relative "human_player"
 
-c = Controller.new(9,2,0)
-# c.set_player(0, Ai1Player)
-# c.set_player(1, Ai1Player)
-c.set_player(0, HumanPlayer)
-c.set_player(1, HumanPlayer)
+opts = Trollop::options do
+  opt :size, "Goban size", :default => 9
+  opt :players, "Number of players", :default => 2
+  opt :ai, "AI plays black"
+  opt :handicap, "Number of handicap stones", :default => 0
+  opt :load, "Game to load like e4,c3,d5", :type => :string
+end
+
+puts opts
+
+# Create controller & players
+c = Controller.new(opts[:size], opts.players, opts.handicap)
+first_human = 0
+if opts.ai
+  c.set_player(0, Ai1Player)
+  first_human = 1
+end
+first_human.upto(opts.players-1) { |n| c.set_player(n, HumanPlayer) }
+
+c.play_moves(opts.load) if opts.load
+
+# Start the game
 c.play_console_game
