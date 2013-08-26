c5fd754eb6cf34e93e0efdae5468e8cbf84ecac4
kubicle <kubicle@yahoo.com>
2013-08-26 19:05:36 +0900

    Added new tests to list

--- test/test_all.rb
+++ test/test_all.rb
@@ -1,4 +1,8 @@
-# Tests all but speed
+# Tests all
 
 require_relative "test_stone"
 require_relative "test_group"
+require_relative "test_board_analyser"
+require_relative "test_zone_filler"
+require_relative "test_sgf_reader"
+require_relative "test_speed"
