667af6921bdee7c785aac14c652459a519b2cb07
kubicle <kubicle@yahoo.com>
2013-09-01 17:57:34 +0900

    TimeKeeper can now be used outside pure tests

--- test/time_keeper.rb
+++ test/time_keeper.rb
@@ -17,7 +17,7 @@ class TimeKeeper
   end
 
   # Call this before start() if you want to compute the ratio automatically
-  # NB: measures will always vary a bit unless we find the perfect calibration test (utopia)
+  # NB: measures will always vary a bit unless we find the perfect calibration code (utopia)
   def calibrate(expected)
     t0 = Time.now
     2000.times do
@@ -28,38 +28,50 @@ class TimeKeeper
     duration = Time.now - t0
     @ratio = duration / expected
     puts "TimeKeeper calibrated at ratio=#{'%.02f' % @ratio} "+
-    	"(ran calibration test in #{'%.03f' % duration} instead of #{expected})"
+    	"(ran calibration in #{'%.03f' % duration} instead of #{expected})"
   end
   
   # Starts timing
   # the expected time given will be adjusted according to the current calibration
-  def start(test_name, expected_in_sec, expected_gc)
-    @test_name = test_name
+  def start(task_name, expected_in_sec, expected_gc)
+    @task_name = task_name
     @expected_time = expected_in_sec * @ratio
     @expected_gc = expected_gc
-    puts "Started test \"#{test_name}\"..." # (expected time #{'%.02f' % @expected_time}s)..."
+    puts "Started \"#{task_name}\"..." # (expected time #{'%.02f' % @expected_time}s)..."
     @gc0 = GC.count
     @t0 = Time.now
   end
 
-  # Stops timing, display the report and throw exception if we went over limit
-  def stop
+  # Stops timing, displays the report and raises exception if we went over limit
+  # Unless raise_if_overlimit is false, in which case we would simply log and return the error message
+  def stop(raise_if_overlimit = true)
     @duration = Time.now - @t0
     @num_gc = GC.count - @gc0
     puts " => "+result_report
-    check_limits
+    check_limits(raise_if_overlimit)
   end
   
   def result_report
     s = ""
-    s << "Test \"#{@test_name}\":"
+    s << "Measuring \"#{@task_name}\":"
     s << " time: #{'%.02f' % @duration}s (expected #{'%.02f' % @expected_time} hence #{'%.02f' % (@duration / @expected_time * 100)}%)"
     s << " GC runs: #{@num_gc} (#{@expected_gc})"
   end
 
 private
-  def check_limits
-    raise "Duration over limit: #{@duration}" if @duration > @expected_time * @tolerance
-    raise "GC run number over limit: #{@num_gc}" if @num_gc > @expected_gc + @gc_tolerance
+
+  def check_limits(raise_if_overlimit)
+    if @duration > @expected_time * @tolerance
+      msg1 = "Duration over limit: #{@duration}" 
+      raise msg1 if raise_if_overlimit
+      return msg1
+    end
+    if @num_gc > @expected_gc + @gc_tolerance
+      msg2 = "GC run number over limit: #{@num_gc}"
+      raise msg2 if raise_if_overlimit
+      return msg2
+    end
+    return ""
   end
+
 end
