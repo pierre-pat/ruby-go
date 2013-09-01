d84c6f7d318240257a63f0c99577f744df24e3fc
kubicle <kubicle@yahoo.com>
2013-09-02 04:21:51 +0900

    switch_side method to use a "consultant heuristic"

--- ai/heuristic.rb
+++ ai/heuristic.rb
@@ -4,11 +4,18 @@
 class Heuristic
 
   def initialize(player)
+    @player = player
     @color = player.color
     @goban = player.goban
     @size = player.goban.size
     @inf = player.inf
     @enemy_colors = player.enemy_colors
+  end
+
+  def switch_side
+    new_color = @enemy_colors.first
+    @enemy_colors = @goban.enemy_colors(@color)
+    @color = new_color
   end
 
 end
