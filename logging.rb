fdfdbb52b991361a8211e35fc28cd1c99f1cb4dc
kubicle <kubicle@yahoo.com>
2013-09-02 01:35:53 +0900

    Separate log flag

--- logging.rb
+++ logging.rb
@@ -8,3 +8,4 @@ $log.level=Logger::DEBUG
 # change $debug to true to see all the debug logs
 # NB: note this slows down everything if $debug is true even if the log level is not DEBUG
 $debug = true
+$debug_group = false
