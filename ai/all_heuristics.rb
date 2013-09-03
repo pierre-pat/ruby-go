33c14fa870792c548a9704a0ac2e26224cbcca26
kubicle <kubicle@yahoo.com>
2013-09-03 15:16:12 +0900

    New heuristics: Pusher, NoEasyPrisoner
    + 1 fix in connector: less prompt to connect if no enemy around

--- ai/all_heuristics.rb
+++ ai/all_heuristics.rb
@@ -3,6 +3,6 @@ require_relative "samples"
 
 class Heuristic
   def Heuristic.all_heuristics
-    return [Spacer, Executioner, Savior, Hunter, Connector]
+    return [Spacer, Executioner, Savior, Hunter, Connector, Pusher, NoEasyPrisoner]
   end
 end
--- ai/samples.rb
+++ ai/samples.rb
@@ -46,6 +46,22 @@ class Spacer < Heuristic
 
 end
 
+class Pusher < Heuristic
+
+  def eval_move(i,j)
+    stone = @goban.stone_at?(i,j)
+    inf = @inf.map[j][i]
+    enemy_inf = 0
+    @enemy_colors.each { |c| enemy_inf += inf[c] }
+    ally_inf = inf[@color]
+    
+    return 0 if enemy_inf == 0 or ally_inf == 0
+    $log.debug("Pusher heuristic sees influences #{ally_inf} - #{enemy_inf} at #{i},#{j}") if $debug
+    return 0.4 * enemy_inf - 0.1 * ally_inf
+  end
+
+end
+
 # Executioner only pray on enemy groups in atari
 class Executioner < Heuristic
 
@@ -75,7 +91,7 @@ class Hunter < Heuristic
       next if 1 == g.all_enemies.each { |e| break(1) if e.lives < g.lives }
       $log.debug("Hunter heuristic (level #{level}) looking at #{i},#{j} threat on #{g}") if $debug
       Stone.play_at(@goban,i,j,@color) # our attack takes one of the 2 last lives (the one in i,j)
-      caught = enemy_is_caught?(g,level)
+      caught = atari_is_caught?(g,level)
       Stone.undo(@goban)
       threat += g.stones.size if caught
     end # each g
@@ -83,30 +99,38 @@ class Hunter < Heuristic
     return 3 * threat
   end
 
-  def enemy_is_caught?(g,level=1)
-    lives = g.all_lives
-    raise "Unexpected: hunter #1" if lives.size != 1
-    last_life = lives.first
-    Stone.play_at(@goban,last_life.i,last_life.j,g.color) # enemy's escape move
+  def atari_is_caught?(g,level=1)
+    all_lives = g.all_lives
+    raise "Unexpected: hunter #1: #{all_lives.size}" if all_lives.size != 1
+    last_life = all_lives.first
+    stone = Stone.play_at(@goban,last_life.i,last_life.j,g.color) # enemy's escape move
     begin
-      return false if g.lives > 2
-      return true if g.lives == 0
-      # g.lives is 1 or 2
-      last_life.neighbors.each do |ally_threatened|
-        next if ally_threatened.color != @color
-        return false if ally_threatened.group.lives < g.lives
-      end
-      return true if g.lives == 1
-      e2 = last_life.empties
-      e2 = g.all_lives if e2.size != 2
-      raise "Unexpected: hunter #2" if e2.size != 2
-      #  recursive descent
-      caught = (eval_move(e2[0].i,e2[0].j,level+1) > 0 or eval_move(e2[1].i,e2[1].j,level+1) > 0)
+      return escaping_atari_is_caught?(stone,level)
     ensure
       Stone.undo(@goban)
     end
   end
 
+  # stone is the atari escape move
+  def escaping_atari_is_caught?(stone,level=1)
+    g = stone.group
+    return false if g.lives > 2
+    return true if g.lives == 0
+    # g.lives is 1 or 2
+    stone.neighbors.each do |ally_threatened|
+      next if ally_threatened.color != @color
+      return false if ally_threatened.group.lives < g.lives
+    end
+    return true if g.lives == 1
+    empties = stone.empties
+    empties = g.all_lives if empties.size != 2
+    raise "Unexpected: hunter #2" if empties.size != 2
+    e1 = empties[0]; e2 = empties[1] # need to keep the empties ref since all_lives returns volatile content
+    #  recursive descent
+    $log.debug("Enemy has 2 lives left: #{e1} and #{e2}") if $debug
+    return (eval_move(e1.i,e1.j,level+1) > 0 or eval_move(e2.i,e2.j,level+1) > 0)
+  end
+  
 end
 
 # Saviors rescue ally groups in atari
@@ -114,8 +138,7 @@ class Savior < Heuristic
 
   def initialize(player)
     super
-    @enemy_hunter = Hunter.new(player)
-    @enemy_hunter.switch_side
+    @enemy_hunter = Hunter.new(player,true)
   end
 
   def eval_move(i,j)
@@ -136,7 +159,7 @@ class Savior < Heuristic
     return 0 if support < 2  # nothing we can do here
     if support == 2
       # when we get 2 lives from the new stone, get our "consultant hunter" to evaluate if we can escape
-      return 0 if @enemy_hunter.enemy_is_caught?(group)
+      return 0 if @enemy_hunter.atari_is_caught?(group)
     end
     $log.debug("=> Savior heuristic thinks we can save a threat of #{threat} in #{i},#{j}") if $debug
     return 3*(threat + support/3.0) # if 2 same size groups, we prefer the easier rescue
@@ -158,7 +181,7 @@ class Connector < Heuristic
     allies = stone.unique_allies(@color)
     num_allies = allies.size
     return 0 if num_allies < 2 # nothing to connect here
-    return 0 if num_enemies == 0 and num_allies == 3 # in this case we never want to connect unless enemy comes by
+    return 0 if num_allies == 3 and num_enemies == 0 # in this case we never want to connect unless enemy comes by
     return 0 if num_allies == 4
     if num_allies == 2
       s1 = s2 = nil; non_unique_count = 0
@@ -167,6 +190,7 @@ class Connector < Heuristic
         s2 = s if s.group == allies[1]
         non_unique_count += 1 if s.color == @color
       end
+      return 0 if non_unique_count == 3 and num_enemies == 0
       # Case of diagonal (strong) stones (TODO: handle the case with a 3rd stone in same group than 1 or 2)
       if non_unique_count == 2 and s1.i != s2.i and s1.j != s2.j
         # No need to connect if both connection points are free
@@ -174,7 +198,38 @@ class Connector < Heuristic
       end
     end
     $log.debug("=> Connector heuristic thinks we should connect in #{i},#{j} (allies:#{num_allies} enemies: #{num_enemies})") if $debug
-    return num_allies * (2 * num_enemies + 1)
+    return 1.0 / @inf.map[j][i][@color] if num_enemies == 0
+    return num_allies * (2 * num_enemies + 1) # TODO: quite random, review it
+  end
+
+end
+
+class NoEasyPrisoner < Heuristic
+
+  def initialize(player)
+    super
+    @enemy_hunter = Hunter.new(player,true)
+  end
+
+  def eval_move(i,j)
+    # TODO: add snapback exception
+    begin
+      stone = Stone.play_at(@goban,i,j,@color)
+      g = stone.group
+      if g.lives == 1
+        $log.debug("NoEasyPrisoner heuristic (backed by Hunter) says #{i},#{j} is foolish") if $debug
+        return -50 * g.stones.size
+      end
+      if g.lives == 2
+        if @enemy_hunter.escaping_atari_is_caught?(stone)
+          $log.debug("NoEasyPrisoner heuristic (backed by Hunter) says #{i},#{j} is foolish") if $debug
+          return -50 * g.stones.size
+        end
+      end
+      return 0 # "all seems fine with this move"
+    ensure
+      Stone.undo(@goban)
+    end
   end
 
 end
