7070445c614598bc7371f8c59b1bc6d1800957c0
kubicle <kubicle@yahoo.com>
2013-09-03 15:13:20 +0900

    Refactored consultant heuristic init

--- ai/heuristic.rb
+++ ai/heuristic.rb
@@ -3,19 +3,18 @@
 # Anything useful for all of them should be stored as data member here.
 class Heuristic
 
-  def initialize(player)
+  def initialize(player,consultant=false)
     @player = player
     @color = player.color
     @goban = player.goban
     @size = player.goban.size
     @inf = player.inf
     @enemy_colors = player.enemy_colors
-  end
-
-  def switch_side
-    new_color = @enemy_colors.first
-    @enemy_colors = @goban.enemy_colors(@color)
-    @color = new_color
+    # For consultant heuristics we reverse the colors
+    if consultant
+      @color = player.enemy_colors.first
+      @enemy_colors = @goban.enemy_colors(@color)
+    end
   end
 
 end
