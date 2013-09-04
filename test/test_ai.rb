f45ebace1ebb4104b7ae1d8fbe762622f1c684b9
kubicle <kubicle@yahoo.com>
2013-09-04 19:49:27 +0900

    Fix AI test to get predictable behavior
    + refact Controller.new

--- test/test_ai.rb
+++ test/test_ai.rb
@@ -11,10 +11,11 @@ require_relative "../ai1_player"
 class TestAi < Test::Unit::TestCase
 
   def init_board(size=9, num_players=2, handicap=0)
-    @controller = Controller.new(size, num_players, handicap)
-    @controller.set_player(0, Ai1Player)
-    @controller.set_player(1, HumanPlayer)
-    @goban = @controller.goban
+    c = @controller = Controller.new
+    c.new_game(size, num_players, handicap)
+    c.set_player(Ai1Player.new(c,BLACK))
+    c.set_player(HumanPlayer.new(c,WHITE))
+    @goban = c.goban
   end
 
   def initialize(test_name)
@@ -34,21 +35,22 @@ class TestAi < Test::Unit::TestCase
     @controller.load_moves("d4,i7,i8,i6,i5,i9,i4,pass")
     assert_equal("h8", @controller.let_ai_play) # h8 is better than killing in h9
     @controller.load_moves("pass")
-    assert_equal("h6", @controller.let_ai_play) # but h7 would have worked too now (ladder)
+    assert_equal("h6", @controller.let_ai_play) # h7 ladder was OK too here but capturing same 2 stones in a ladder
+    # the choice between h6 and h7 is decided by smaller differences like distance to corner, etc.
     @controller.load_moves("h7")
     assert_equal("g7", @controller.let_ai_play)
   end
 
   def test_hunter_2
     # Ladder
-    # 9 ++++++++@
+    # 9 O+++++++@
     # 8 ++++++++@
     # 7 ++++++++O
     # 6 ++++++++O
     # 5 ++++++++@
     # 4 ++++++++@
     #   abcdefghi
-    @controller.load_moves("i9,i7,i8,i6,i5,pass,i4,pass")
+    @controller.load_moves("i9,i7,i8,i6,i5,a9,i4,pass")
     assert_equal("h7", @controller.let_ai_play)
     @controller.load_moves("h6")
     assert_equal("g6", @controller.let_ai_play)
@@ -61,19 +63,20 @@ class TestAi < Test::Unit::TestCase
 
   def test_hunter_3
     # Ladder breaker (f4)
-    # 9 ++++++++@
+    # 9 O+++++++@
     # 8 ++++++++@
     # 7 ++++++++O
     # 6 ++++++++O
     # 5 ++++++++@
     # 4 +++++O++@
     #   abcdefghi
-    # Where the AI plays depends on other heuristics; 
-    # what is sure is that neither h6 nor h7 works.
-    @controller.load_moves("i9,i7,i8,i6,i5,f4,i4,pass")
+    # AI should prefer to eat 1 stone in a9 since the ladder fails.
+    # What is sure is that neither h6 nor h7 works.
+    @controller.load_moves("i9,i7,i8,i6,i5,f4,i4,a9")
     move = @controller.let_ai_play
     assert_not_equal("h7", move)
     assert_not_equal("h6", move)
+    assert_equal(true, (move == "b9" or move == "a8"))
   end
   
 end
